dae8bad13cedef04fa58c0916b53de18b9477d3d
Assault1892 <vvvvvgaga12356@yahoo.co.jp>
2025-01-08 12:06:06 +0900

    for check

--- temp.py
+++ temp.py
@@ -1,14 +1,6 @@
-g_flag = True
-print("関数実行前フラグ: " + str({g_flag}))
+import json
+with open("loader.json") as f:
+    loader = json.load(f)
 
-
-def func(flag):
-    global g_flag
-    print("現在のフラグ: " + str({g_flag}))
-    print("フラグ反転！")
-    g_flag = not flag
-    print("現在のフラグ: " + str({g_flag}))
-
-
-func(g_flag)
-print("関数実行後フラグ: " + str({g_flag}))
+for i in loader["loader"]:
+    print(i)
